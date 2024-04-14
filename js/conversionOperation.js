f5cdd9bba88bc8d364a32759973dd2e0e90cc54d
kupendrav <kupendrav99@gmail.com>
2024-04-14 13:10:36 +0530

    added: investigation conversionOperation

---
+++ js/conversionOperation.js
@@ -0,0 +1,13 @@
+let case1 = '50abc';
+let newcase1 = Number(case1);
+console.log(typeof newcase1,newcase1);
+// "33"= 33
+// "33abc"=NaN
+// true =1 ,false=0
+
+let kickIn = "goal"
+let booleanKickIn = Boolean(kickIn);
+console.log(booleanKickIn);
+// 1=> true,0=> false;
+// ""=> false
+// "any value" =>true
