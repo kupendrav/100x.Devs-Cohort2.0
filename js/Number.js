c92f97334174cc10be3414b9cc471b4bdbeb4282
kupendrav <kupendrav99@gmail.com>
2024-04-20 21:58:20 +0530

    added: summary of Numbers and maths in js

--- js/Number.js
+++ js/Number.js
@@ -3,3 +3,7 @@ console.log(score.toPrecision(3)); // 26.9
 const hundreds = 100000000;
 console.log(hundreds.toLocaleString('en-UK'));  // 100,000,000
 console.log(hundreds.toLocaleString('en-IN'));  // 10,00,00,000
+// ++++++++++++++------------------++++++++++++++++++++++++
+
+//  properties are that we directly acces by . notation
+//  methods are that we call by () notation
