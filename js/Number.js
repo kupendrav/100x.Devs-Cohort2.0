7f1ad9c58f3657535fc1aff38f1f6da428ea9c85
kupendrav <kupendrav99@gmail.com>
2024-04-20 21:54:28 +0530

    added: summary of Numbers and maths in js

---
+++ js/Number.js
@@ -0,0 +1,5 @@
+const score = 26.9845;
+console.log(score.toPrecision(3)); // 26.9
+const hundreds = 100000000;
+console.log(hundreds.toLocaleString('en-UK'));  // 100,000,000
+console.log(hundreds.toLocaleString('en-IN'));  // 10,00,00,000
