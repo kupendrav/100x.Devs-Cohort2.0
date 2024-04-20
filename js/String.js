4a9e93087ddeb8bee5318f4ef62a6bc21cc68cff
kupendrav <kupendrav99@gmail.com>
2024-04-20 18:03:31 +0530

    added:String file and methods of strings and there properties

---
+++ js/String.js
@@ -0,0 +1,10 @@
+let myName = "Johnvarghesekhan";
+console.log(myName.length); // 4 
+
+console.log(`my name is ${myName}`)
+// +++++++++++++++++++
+/** should go through the methods in strings refer the mdn docs */
+// +++++++++++++++++++
+const myNewName = myName.slice(1, 7);
+console.log(myNewName); // John
+
