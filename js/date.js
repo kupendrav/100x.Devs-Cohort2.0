badfaa7c53015c4b7c28db8ac3a5254aa13989e6
kupendrav <kupendrav99@gmail.com>
2024-04-21 12:48:44 +0530

    Refactor code to improve performance and readability

---
+++ js/date.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+
+// Date is OBJECT in js
+// moth in js start from 0 
+
+// creating new date
+let myCreateDate = new Date(2024,0,29)
+console.log(myCreateDate.toDateString());
