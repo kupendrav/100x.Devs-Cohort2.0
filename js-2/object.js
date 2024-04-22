7edca7dcbe4252f8eea4a0f1b2f931012257e05f
kupendrav <kupendrav99@gmail.com>
2024-04-22 13:31:26 +0530

    Update object.js with symbol usage and object immutability

--- js-2/object.js
+++ js-2/object.js
@@ -14,3 +14,13 @@ console.log(typeof(myinfo.mobileNo));
 // how to use symbol data type in object
 
 // define the symbol key first and then use it in object
+// we can change the value in the object by simply = 
+myinfo.name = "kupendra kumar";
+console.log(myinfo);
+
+// we can freeze the object so that we can't change the value of the object properties using dot notation or bracket notation
+
+Object.freeze(myinfo); // this will make the whole object immutable
+
+myinfo.name = "harshavardhana"
+console.log(myinfo);
