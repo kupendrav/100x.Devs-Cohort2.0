c1560d0dcb12f2e777715ec544dee7c5de81f096
kupendrav <kupendrav99@gmail.com>
2024-04-22 09:49:54 +0530

    Refactor array concatenation using spread operator

--- js-2/arrays2.js
+++ js-2/arrays2.js
@@ -1,5 +1,8 @@
 const mySweets = ["halwa","laddoo","jalebi","gulab jamun","barfi","rasgulla","peda","kaju katli"]
 
 const myFruits = ["apple","banana","mango","orange","grapes"]
-const allItems = mySweets.concat(myFruits);
-console.log(allItems);
+// const allItems = mySweets.concat(myFruits);
+// console.log(allItems);
+
+const allFruitsSweets = [...mySweets, ...myFruits];  //using spread operator to combine the arrays
+console.log(allFruitsSweets);
