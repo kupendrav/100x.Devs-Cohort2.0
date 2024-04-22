5f26f4e4aee0e9a1f799bd76bd566f9a64d30d62
kupendrav <kupendrav99@gmail.com>
2024-04-22 09:57:51 +0530

    Add array manipulation and conversion methods

--- js-2/arrays2.js
+++ js-2/arrays2.js
@@ -6,3 +6,10 @@ const myFruits = ["apple","banana","mango","orange","grapes"]
 
 const allFruitsSweets = [...mySweets, ...myFruits];  //using spread operator to combine the arrays
 console.log(allFruitsSweets);
+
+const another_array = [1,2,3,[4,5],6,7,[8,[9,10]]]
+let flatArray = another_array.flat(Infinity);
+console.log(flatArray); // flat method is used to flatten the array to the specified depth. Infinity is used to flatten the array to any depth.
+
+console.log(Array.from('kupendra'))
+// from method is used to create a new shallow-copied Array instance from an array-like or iterable object. It makes it easy to convert other data structures like strings and objects into arrays.
