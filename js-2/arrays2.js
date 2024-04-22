8c8bc3325b7d7134c27f429c7ba92522893d01df
kupendrav <kupendrav99@gmail.com>
2024-04-22 09:44:29 +0530

    Add myFruits array and concatenate it with mySweets array

--- js-2/arrays2.js
+++ js-2/arrays2.js
@@ -1,2 +1,5 @@
 const mySweets = ["halwa","laddoo","jalebi","gulab jamun","barfi","rasgulla","peda","kaju katli"]
 
+const myFruits = ["apple","banana","mango","orange","grapes"]
+const allItems = mySweets.concat(myFruits);
+console.log(allItems);
