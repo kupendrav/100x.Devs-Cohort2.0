b246cb79f83c6c6822f78c5cecd6ff4905b5b892
kupendrav <kupendrav99@gmail.com>
2024-04-26 18:50:15 +0530

    Add BMI category display in pro2.js

--- js-projects/project-2/pro2.js
+++ js-projects/project-2/pro2.js
@@ -12,6 +12,19 @@ form.addEventListener('submit', function (e) {
     alert('Please provide a valid weight')
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    document.querySelector('#bmi').innerHTML = `<span>Your BMI is ${bmi}</span>`;
+   document.querySelector('#bmi').innerHTML = `<span>Your BMI is ${bmi}</span>`;
+   // determine and display the user's BMI category
+   if(bmi < 18.5) {
+    document.querySelector('#result').innerHTML = `<span>You are underweight🥲</span>`;
+  } else if(bmi >= 18.5 && bmi <= 24.9) {
+    document.querySelector('#result').innerHTML = `<span>You are normal weight😁</span>`;
+  } else if(bmi >= 25 && bmi <= 29.9) {
+    document.querySelector('#result').innerHTML = `<span>You are overweight🧐</span>`;
+  } else {
+    document.querySelector('#result').innerHTML = `<span>You are obese 😱</span>`;
   }
+  }
+
+  
 });
+
