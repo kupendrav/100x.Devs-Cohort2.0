80824b74797853b8a75e1a2402925dab4b2bd6cb
kupendrav <kupendrav99@gmail.com>
2024-04-26 14:29:41 +0530

    Refactor button event listeners and add missing semicolons

--- js-projects/project1/pro1.js
+++ js-projects/project1/pro1.js
@@ -1,22 +1,10 @@
-const buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll('.button')
 
-const body = document.querySelector('body');
+let body = document.querySelector('body')
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = 'blue';
-    } else if (e.target.id === 'red') {
-      body.style.backgroundColor = 'red';
-    } else if (e.target.id === 'green') {
-      body.style.backgroundColor = 'green';
-    } else if (e.target.id === 'yellow') {
-      body.style.backgroundColor = 'yellow';
-    } else if (e.target.id === 'cyan') {
-      body.style.backgroundColor = 'cyan';
-    }
-     else if (e.target.id === 'black') {
-      body.style.backgroundColor = 'black';
-    }
+buttons.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    let color = e.target.id
+    body.style.backgroundColor = color
   })
 });
