9e917ff8472f013b98e736f72c37e5631f4a715b
kupendrav <kupendrav99@gmail.com>
2024-04-28 00:24:28 +0530

    Update pro4.html and pro4.js files

--- js-projects/project-4/pro4.js
+++ js-projects/project-4/pro4.js
@@ -15,3 +15,16 @@ document.querySelector('button').addEventListener('click', () => {
   speech.text = document.querySelector('textarea').value;
   window.speechSynthesis.speak(speech);
 });
+
+
+
+// download the voice generated 
+document.querySelector('#download').addEventListener('click', () => {
+  const blob = new Blob([speech.text], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'voice.mp3';
+  a.click();
+  URL.revokeObjectURL(url);
+});
