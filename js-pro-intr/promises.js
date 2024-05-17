4b2a6a9e135e90025c7d8fb5105535ba296a39b0
Kupendra V <93828746+kupendrav@users.noreply.github.com>
2024-05-17 10:09:19 +0000

    added:uses of promises/ create a promise

--- js-pro-intr/promises.js
+++ js-pro-intr/promises.js
@@ -9,3 +9,13 @@ setTimeout(()=>{
 promiseOne.then(()=>{
     console.log("promise consumed");
 })
+
+const promiseTwo = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve({username:"chai", email:"abc@gmail.com"})
+    },1000)
+})
+
+promiseTwo.then((user)=>{
+    console.log(user);
+})
