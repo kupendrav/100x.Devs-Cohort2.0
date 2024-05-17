aa275641c97b951264a482be916bd39781167417
Kupendra V <93828746+kupendrav@users.noreply.github.com>
2024-05-17 17:07:22 +0000

    added:oop.js

--- js-classes-oop/oop.js
+++ js-classes-oop/oop.js
@@ -4,3 +4,18 @@ const user = {
     signedOut: true
 }
 console.log(this);
+
+// function User(username, loginCount , isLoggedIn){
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.isLoggedIn = isLoggedIn;
+
+//     return this
+// }
+
+// const userOne = User("hitesh",3,true)
+// const userTwo = User("hitesh",3,true)
+
+// console.log(userOne);
+
+/**overites the value of userone though usertwo is not printed */
