ffd231884e8c4deff5d68872119a93640d7e0bb0
kupendrav <kupendrav99@gmail.com>
2024-06-16 17:04:30 +0530

    added: into folder

--- js/variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 1999;
-let accountEmail = "abc@gmail.com";
-var accountPassword = '1234';
-accountCity='banglore';
-
-// accountId = 3;
-// console.log(accountId);
-console.table([accountId,accountEmail.accountPassword,accountCity]);
