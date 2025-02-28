d693f07f4ba1217563557460377a45b3da78ab53
kupendrav <kupendrav99@gmail.com>
2025-02-28 21:55:59 +0530

    added: implement account details logging and variable type documentation

--- micro-js/micro.js
+++ micro-js/micro.js
@@ -1,1 +1,16 @@
-alert('hello')
+const accountId = 1999;
+let accountEmail = "abc@fmail.com"
+var accountPassword = '1234';
+
+accountCity='banglore';
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+// there is something called ecma standard 
+// types of variable let and const 
+// types of variables =
+/** .1.number
+ * 2.string
+ * 3.boolean
+ * 4.null
+ * 5.undefined
+ * 6.symbol
+ */
