a168589c760a8fc22947809f905670d82b058a6c
kupendrav <kupendrav99@gmail.com>
2025-03-02 22:27:12 +0530

    added: enhance micro.js with comparison examples and documentation of primitive and non-primitive data types

--- micro-js/micro.js
+++ micro-js/micro.js
@@ -27,3 +27,29 @@ console.log(valueInNumber);
 console.log(+true);
 console.log(+"");
 
+//comparision
+console.log(3>2);
+console.log(null > 0);
+// comparison is diiferent from equality 
+
+/** primitive
+ * 7 types of primitive data types
+ * 1.string
+ * 2.number
+ * 3.boolean
+ * 4.null
+ * 5.undefined
+ * 6.symbol
+ * 7.bigint
+ 
+ * non primitive or reference data types
+ * 1.object
+ * 2.array
+ * 3.function
+ * 4.date
+ * 5.regular expression
+ * 6.map
+ * 7.set
+ */
+const score = 100;
+const scoreValue = 100.3;
